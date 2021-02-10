4c85c7d96696b62df414ee1509aa73b5ecd68d93
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2021-02-10 10:48:35 +0900

    ...

---
+++ back-end-3/model/contentRepo.js
@@ -0,0 +1,55 @@
+let mysql_async = require('mysql');
+// let mysql = require('sync-mysql');
+// let db = new mysql({
+//   host: 'localhost',
+//   user: 'nodejs',
+//   password: '111111',
+//   database: 'opentutorials'
+// });
+
+class ContentRepo
+{
+  static db_async = mysql_async.createConnection({
+    host: 'localhost',
+    user: 'nodejs',
+    password: '111111', database: 'opentutorials'
+  });
+
+  query_get()
+  {
+    return `SELECT * FROM topic ORDER BY id DESC LIMIT 0, 5`;
+  }
+  query_get_content_5(numOfLoadedContents)
+  {
+    return `SELECT * FROM topic ORDER BY id DESC LIMIT ${numOfLoadedContents},5`;
+  }
+
+  // NOTE: 뭔가 오류가 있음. 수정 필요.
+  query_get_by_search(nKeyWord)
+  {
+    return `SELECT * FROM topic ORDER BY id DESC WHERE (title LIKE '%${nKeyWord}%' OR description LIKE '%${nKeyWord}%')`;
+  }
+
+  query_get_partial_by_search(nKeyWord, nLoadedContents)
+  {
+    return `SELECT * from topic ORDER BY id DESC WHERE (title LIKE '%"${nKeyWord}"%' OR description LIKE '%"${nKeyWord}"% LIMIT ${nLoadedContents},5')`;
+  }
+
+  query_post(title, desc, user)
+  {
+    return `INSERT INTO topic (title, description, created, author_id) VALUES("${title}", "${desc}", NOW(), ${user})`;
+  }
+  
+  contentForm(id, title, desc, user, date)
+  {
+    return {
+      'id': id,
+      'title': title,
+      'content': desc,
+      'user': user,
+      'date': date
+    };
+  }
+}
+
+module.exports = ContentRepo;
--- back-end-3/routes/content.js
+++ back-end-3/routes/content.js
@@ -4,6 +4,9 @@ var template = require('../lib/template');
 
 var boardData = require('../data/boardData');
 
+let contentRepo = require('../model/contentRepo');
+const contentRepoImpl = new contentRepo();
+
 // DB
 var mysql_async = require('mysql');
 var db_async = mysql_async.createConnection({
@@ -12,8 +15,7 @@ var db_async = mysql_async.createConnection({
   password: '111111', database: 'opentutorials'
 });
 
-var gid = boardData.contents.length;
-
+// 결과를 역순으로 저장.
 function createResponseContentArray(results)
   {
     var nNumContent = results.length;
@@ -26,17 +28,13 @@ function createResponseContentArray(results)
     // console.log(results);
     for (var i = nLoopStart; i >= nLoopEnd; i--)
     {
-      var content = {
-        'id': results[i].id,
-        'title': results[i].title,
-        'content': results[i].description,
-        'user': results[i].author_id,
-        'date': results[i].created
-      };
-      contents.push(content);
+      contents.push(contentRepoImpl.contentForm(
+        results[i].id,
+        results[i].title,
+        results[i].description,
+        results[i].author_id,
+        results[i].created));
     }
-
-    // console.log(contents);
 
     const response = {
       statusCode: 200,
@@ -55,7 +53,7 @@ function createResponseContentArray(results)
 router.get('/', function(req, res, next){
   // console.log('contents get');
   // 2. 서버 재시작 후 게시글 추가 확인(SELECT문)
-  db_async.query(`SELECT * FROM topic`, function(err, results, fields){
+  db_async.query(contentRepoImpl.query_get(), function(err, results, fields){
     
     if (err)
     {
@@ -64,32 +62,45 @@ router.get('/', function(req, res, next){
     }
     else
     {
-      var response = createResponseContentArray(results);
-      res.send(response);
+      // var response = createResponseContentArray(results);
+      // res.send(response);
+      var contents = [];
+      console.log(results.length);
+      for (let result of results)
+      {
+        contents.push(contentRepoImpl.contentForm(
+          result.id,
+          result.title,
+          result.description,
+          result.author_id,
+          result.created
+        ));
+      }
+      console.log(contents);
+      res.send(contents);
     }
   });
 });
 
 router.get('/loadpartialcontent/:numOfLoadedContents', function(req, res, next){
   var numOfLoadedContents = req.params.numOfLoadedContents;
-
-  db_async.query(`SELECT * FROM topic LIMIT ${numOfLoadedContents},5`, function (err, results, fields) {
+  db_async.query(contentRepoImpl.query_get_content_5(numOfLoadedContents), function (err, results, fields) {
     console.log("numOfLoadedContents: " + numOfLoadedContents);
     if (err) {
       console.log(err);
       res.sendStatus(403);
     }
     else {
-      var contents = [];
-      for (var i = 0; i< results.length; i++) {
-        var content = {
-          'id': results[i].id,
-          'title': results[i].title,
-          'content': results[i].description,
-          'user': results[i].author_id,
-          'date': results[i].created
-        };
-        contents.push(content);
+      let contents = [];
+      for (let result of results)
+      {
+        contents.push(contentRepoImpl.contentForm(
+          result.id, 
+          result.title, 
+          result.description,
+          result.author_id,
+          result.created
+          ));
       }
       console.log(contents);
 
@@ -113,22 +124,20 @@ router.get('/loadpartialcontent/:numOfLoadedContents/keyword/:keyWord', function
   var nKeyWord = req.params.keyWord;
   var numOfLoadedContents = req.params.numOfLoadedContents;
   console.log("keyword: " + nKeyWord + ', numOfLoadedContents: ' + numOfLoadedContents);
-  db_async.query(`SELECT * from topic WHERE (title LIKE '%${nKeyWord}%' OR description LIKE '%${nKeyWord}% LIMIT ${numOfLoadedContents},5')`, function(error, results, fields){
+  db_async.query(contentRepoImpl.query_get_partial_by_search(nKeyWord, numOfLoadedContents), function(error, results, fields){
     if (error) {
-      console.log(error);
+      console.log("error: " + error);
       res.sendStatus(403);
     }
     else {
-      contents = []
-      for (var i = 0; i < results.length; i++) {
-        var content = {
-          'id': results[i].id,
-          'title': results[i].title,
-          'content': results[i].description,
-          'user': results[i].author_id,
-          'date': results[i].created
-        };
-        contents.push(content);
+      let contents = []
+      for (result of results) {
+        contents.push(contentRepoImpl.contentForm(
+          result.id,
+          result.title,
+          result.description,
+          result.author_id,
+          result.created));
       }
       console.log(contents);
 
@@ -149,7 +158,7 @@ router.get('/loadpartialcontent/:numOfLoadedContents/keyword/:keyWord', function
 
 // 1. 게시글 추가하기(INSERT문)
 router.post('/', function(req, res, next){
-  // console.log('contents post');
+  console.log('[content post]');
   var data = JSON.parse(req.body.body);
 
   if (data.title === '')
@@ -165,8 +174,8 @@ router.post('/', function(req, res, next){
   else
   {
     console.log(data.title, data.content);
-    db_async.query(`INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`,
-    [data.title, data.content, data.user], function (error, results) {
+    console.log(data.title, data.content);
+    db_async.query(contentRepoImpl.query_post(data.title, data.content, data.user), function (error, results) {
       if (error) {
         throw error
         // console.log("post fail 3");
@@ -181,15 +190,6 @@ router.post('/', function(req, res, next){
         console.log(date_form);
 
         console.log('[post] ' + data);
-        var hellodata = {
-          "id": gid++,
-          "title": data.title,
-          "content": data.content,
-          "date": date_form,
-          "user": data.user,
-          "pw": data.pw
-        }
-        console.log(hellodata);
 
         res.send("post success");
       }
@@ -208,18 +208,19 @@ router.get('/:contentId', function(req, res, next){
     else
     {
       var id = req.params.contentId;
-      var idx = getJsonListIndexFromId(id);
-  
-      console.log(results[0]);
+      console.log("여기요: " + id);
+      console.log("results[0]: " + results[0]);
       console.log(results[0].description);
-      hello = {
-        title: results[0].title,
-        content: results[0].description,
-        date: results[0].created,
-        user: results[0].author_id,
-      }
-
-      res.send(JSON.stringify(hello));
+      let result = results[0];
+      console.log("여기요2: " + result.id);
+      let content = contentRepoImpl.contentForm(
+        id, // ..?
+        result.title,
+        result.description,
+        result.author_id,
+        result.created);
+      console.log("content: " + content.id);
+      res.send(content);
     }
   });
 });
@@ -237,16 +238,17 @@ router.put('/:contentId', function(req, res, next){
   // 테이블 데이터 일부 수정
   console.log('put /:contentId');
   var id = req.params.contentId;
-  var idx = getJsonListIndexFromId(id);
   var json = JSON.parse(req.body.body);
-  console.log('json: ' + json);
+  console.log("title: " + json.title);
+  console.log("content: " + json.content);
+  console.log("user: " + json.user);
+  console.log("id: " + json.id);
 
   // console.log("id:", id, ", user:", json.user, json.pw, json.title, json.content);
-  db_async.query(`UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?`, [json.title, json.content, id], function(error, results){
+  db_async.query(`UPDATE topic SET title=?, description=?, author_id=? WHERE id=?`, [json.title, json.content, json.user, id], function(error, results){
 
     if (error)
     {
-      
       console.log(403);
     }
     else
@@ -310,37 +312,17 @@ router.delete('/:contentId', function(req, res, next) {
       res.sendStatus(200);
     }
   });
-  // console.log('delete');
-  // var id = req.params.contentId;
-  // console.log(id);
-  // var idx = getJsonListIndexFromId(id);
-
-  // var json = JSON.parse(req.body.body);
-  // console.log(json);
-
-  // var statusCode = identifiy_(id, json.user, json.pw);
-
-  // if (statusCode === 200)
-  // {
-  //   if (idx >= 0)
-  //   {
-  //     boardData.contents.splice(idx, 1);
-  //   }
-  // }
-
-  // res.send(statusCode);
- 
 });
 
 router.get('/search/:keyWord', function(req, res, next)
 {
+  console.log('/search/:keyWord');
   var nKeyWord = req.params.keyWord;
   console.log(nKeyWord);
-  // nKeyWord를 포함하는 데이터 긁어와야 함..!
-  db_async.query(`SELECT * from topic WHERE (title LIKE '%${nKeyWord}%' OR description LIKE '%${nKeyWord}%')`, function(error, results){
+  db_async.query(contentRepoImpl.query_get_by_search(nKeyWord), function(error, results){
     if (error)
     {
-      console.log(error);
+      console.log("error: " + error);
       res.sendStatus(403);
     }
     else
